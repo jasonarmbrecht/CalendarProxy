5f41cd9d5c58a95fe122a51d89d59eade36c12a9
jasonarmbrecht <jason.armbrecht@gmail.com>
2023-08-27 13:05:02 +0930

    Update proxy.js
    added timestamp (ACST only) and shortnened URL for cleaner terminal output.

--- proxy.js
+++ proxy.js
@@ -9,9 +9,24 @@ const PORT = 8080;
 // set allowed domain (only 1)
 const ALLOWED_DOMAIN = 'airmaestro.cobhamspecialmission.com.au';
 
+// generate ACST timestamp
+function getACSTTimestamp() {
+    const now = new Date();
+    const acstOptions = { timeZone: 'Australia/Darwin', hour12: false };
+    return now.toLocaleString('en-AU', acstOptions);
+  }
+
+// shorten URL for terminal output
+function shortenUrl(url) {
+    const domain = 'airmaestro';
+    const lastCharacters = url.slice(-5);
+    return `${domain}...${lastCharacters}`;
+  }
+
 // log IP addresses
 app.use((req, res, next) => {
-    console.log(`Request from IP address: ${req.ip}`);
+    const timestamp = getACSTTimestamp();
+    console.log(`[${timestamp}] Request from IP address: ${req.ip}`);
     next();
   });
 
@@ -22,13 +37,18 @@ app.use('/proxy/*', (req, res) => {
   if (targetUrl.startsWith('webcals://')) {
     targetUrl = 'https://' + targetUrl.slice('webcals://'.length);
   }
-  // assess if crafted url has the allowable domain, then execute proxy request.
+  // create short URL
+  const shortenedUrl = shortenUrl(targetUrl);
+
+  // assess if crafted url has the allowable domain, then execute proxy request
   if (targetUrl.includes(ALLOWED_DOMAIN)) {
     req.pipe(request(targetUrl)).pipe(res);
-    // error handling
-    console.log(`Proxying request for: ${targetUrl}`);
+    // error handling and logging
+    const timestamp = getACSTTimestamp();
+    console.log(`[${timestamp}] Proxying request for: ${shortenedUrl}`);
   } else {
-    console.log(`Access denied for: ${targetUrl}`);
+    const timestamp = getACSTTimestamp();
+    console.log(`[${timestamp}] Access denied for: ${shortenedUrl}`);
     res.status(403).send('Access forbidden');
   }
 });
