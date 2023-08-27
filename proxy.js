062c543a63bf46f979a8d812aa1afaaeb85b0b25
jasonarmbrecht <jason.armbrecht@gmail.com>
2023-08-27 13:42:19 +0930

    Update proxy.js
    added logging. tidied up a few things.

--- proxy.js
+++ proxy.js
@@ -1,6 +1,7 @@
 // add libaries
 const express = require('express');
 const request = require('request');
+const fs = require('fs');
 
 // use express
 const app = express();
@@ -14,7 +15,7 @@ function getACSTTimestamp() {
     const now = new Date();
     const acstOptions = { timeZone: 'Australia/Darwin', hour12: false };
     return now.toLocaleString('en-AU', acstOptions);
-  }
+}
 
 // shorten URL for terminal output
 function shortenUrl(url) {
@@ -22,36 +23,48 @@ function shortenUrl(url) {
     const firstCharacters = afterHttps ? afterHttps[1] : '';
     const lastCharacters = url.slice(-5);
     return `${firstCharacters}...${lastCharacters}`;
-  }
+}
 
 // log IP addresses
 app.use((req, res, next) => {
     const timestamp = getACSTTimestamp();
     console.log(`[${timestamp}] Request from IP address: ${req.ip}`);
     next();
-  });
+});
+
+app.use((req, res, next) => {
+    const timestamp = getACSTTimestamp();
+    const ip = req.ip.replace('::ffff:', ''); 
+    const logEntry = `[${timestamp}] IP: ${ip}, URL: ${req.originalUrl}\n`;
+    fs.appendFile('proxy.log', logEntry, (error) => {
+        if (error) {
+          console.error('Error writing to log file:', error);
+        }
+    });
+    next();
+});
 
 // setup url pattern (/proxy/wildcard)
 app.use('/proxy/*', (req, res) => {
-  let targetUrl = req.params[0];
+    let targetUrl = req.params[0];
   // if user accidentally pastes webcals link, automatically convert to https
-  if (targetUrl.startsWith('webcals://')) {
-    targetUrl = 'https://' + targetUrl.slice('webcals://'.length);
-  }
+    if (targetUrl.startsWith('webcals://')) {
+        targetUrl = 'https://' + targetUrl.slice('webcals://'.length);
+    }
   // create short URL
   const shortenedUrl = shortenUrl(targetUrl);
 
   // assess if crafted url has the allowable domain, then execute proxy request
-  if (targetUrl.includes(ALLOWED_DOMAIN)) {
-    req.pipe(request(targetUrl)).pipe(res);
+    if (targetUrl.includes(ALLOWED_DOMAIN)) {
+        req.pipe(request(targetUrl)).pipe(res);
     // error handling and logging
-    const timestamp = getACSTTimestamp();
-    console.log(`[${timestamp}] Proxying request for: ${shortenedUrl}`);
-  } else {
-    const timestamp = getACSTTimestamp();
-    console.log(`[${timestamp}] Access denied for: ${shortenedUrl}`);
-    res.status(403).send('Access forbidden');
-  }
+        const timestamp = getACSTTimestamp();
+        console.log(`[${timestamp}] Proxying request for: ${shortenedUrl}`);
+    } else {
+        const timestamp = getACSTTimestamp();
+        console.log(`[${timestamp}] Access denied for: ${shortenedUrl}`);
+        res.status(403).send('Access forbidden');
+    }
 });
 
 // start the proxy
